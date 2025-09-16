6e3940d87732e8f53a06c47bb8441b3b189e7e61
lilpressf <daanwelten05@gmail.com>
2025-09-16 11:55:24 +0200

    basic terraform code for testing pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+# VPC and Subnet
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "basic-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances
+resource "aws_instance" "web1" {
+  ami           = "ami-0a91cd140a1fc148a" # Amazon Linux 2 (eu-central-1)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "web1"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "ami-0a91cd140a1fc148a"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "web2"
+  }
+}
+

43ddc6316b5e4c8573e3054e6924031ab18e4469
lilpressf <daanwelten05@gmail.com>
2025-09-19 12:45:15 +0200

    Changed DNS name again

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 # Private Route 53 hosted zone
 resource "aws_route53_zone" "private" {
-  name = "internal.daan.local"
+  name = "internal.daan.internal"
 
   vpc {
     vpc_id = aws_vpc.main.id
@@ -10,7 +10,7 @@ resource "aws_route53_zone" "private" {
 # Private DNS record for web1
 resource "aws_route53_record" "web1_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "web1.internal.daan.local"
+  name    = "web1.internal.daan.internal"
   type    = "A"
   ttl     = 300
   records = [aws_instance.web1.private_ip]
@@ -19,16 +19,16 @@ resource "aws_route53_record" "web1_private" {
 # Private DNS record for web2
 resource "aws_route53_record" "web2_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "web2.internal.daan.local"
+  name    = "web2.internal.daan.internal"
   type    = "A"
   ttl     = 300
   records = [aws_instance.web2.private_ip]
 }
 
-# Private DNS record for RDS (CNAME instead of A)
+# Private DNS record for RDS
 resource "aws_route53_record" "db_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "db.internal.daan.local"
+  name    = "db.internal.daan.internal"
   type    = "CNAME"
   ttl     = 300
   records = [aws_db_instance.db.endpoint]

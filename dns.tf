15c187a6ac66dd7e3cbd5b2a798a2436c40b2cd7
lilpressf <daanwelten05@gmail.com>
2025-09-19 13:00:08 +0200

    DNS try 3

--- dns.tf
+++ dns.tf
@@ -1,4 +1,3 @@
-# Private Route 53 hosted zone
 resource "aws_route53_zone" "private" {
   name = "internal.daan.internal"
 
@@ -7,28 +6,25 @@ resource "aws_route53_zone" "private" {
   }
 }
 
-# Private DNS record for web1
 resource "aws_route53_record" "web1_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "web1.internal.daan.internal"
+  name    = "web1"              # relative to zone
   type    = "A"
   ttl     = 300
   records = [aws_instance.web1.private_ip]
 }
 
-# Private DNS record for web2
 resource "aws_route53_record" "web2_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "web2.internal.daan.internal"
+  name    = "web2"
   type    = "A"
   ttl     = 300
   records = [aws_instance.web2.private_ip]
 }
 
-# Private DNS record for RDS
 resource "aws_route53_record" "db_private" {
   zone_id = aws_route53_zone.private.zone_id
-  name    = "db.internal.daan.internal"
+  name    = "db"
   type    = "CNAME"
   ttl     = 300
   records = [aws_db_instance.db.endpoint]

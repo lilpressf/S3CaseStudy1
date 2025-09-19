6a5fb64415a5422328fa3b05afaad3f7afe23ec6
lilpressf <daanwelten05@gmail.com>
2025-09-19 12:17:24 +0200

    Fixing DNS issue

--- dns.tf
+++ dns.tf
@@ -25,11 +25,11 @@ resource "aws_route53_record" "web2_private" {
   records = [aws_instance.web2.private_ip]
 }
 
-# Private DNS record for RDS
+# Private DNS record for RDS (CNAME instead of A)
 resource "aws_route53_record" "db_private" {
   zone_id = aws_route53_zone.private.zone_id
   name    = "db.internal.daan.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 300
   records = [aws_db_instance.db.endpoint]
 }
